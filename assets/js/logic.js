54979931f8ea8b5cde84558d53a4f6334ca4c156
juan uribe <juanpablouribe@mac.com>
2019-10-24 20:35:19 -0400

    Bug corrected, minutes value for next train time resets if value is > 60

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,25 +9,41 @@ $(document).ready(function () {
         var frequency = $("#input-frequency").val().trim();
         // Convert first train hour to minutes
         var firstTrainHoursMinutesArr = firstTrain.split(":", 2);
+        console.log("split of first train time is :" + firstTrainHoursMinutesArr)
         var firstTrainHours = parseInt(firstTrainHoursMinutesArr[0]);
+        console.log("hour of first train is: " + firstTrainHours)
         var firstTrainMinutes = parseInt(firstTrainHoursMinutesArr[1]);
+        console.log("minutes of first train is: " + firstTrainMinutes)
         var firstTrainHoursToMinutes = firstTrainHours * 60
+        console.log("first train hours converted to minutes is: "+ firstTrainHoursToMinutes) 
         var firstTrainMinutesTotal = (firstTrainHoursToMinutes) + firstTrainMinutes
+        console.log("first train time in minutes is: " + firstTrainMinutesTotal)
         // Convert present time to minutes
         var timeNow = new Date();
+        console.log("the time now is: " + timeNow)
         var getHours = timeNow.getHours();
+        console.log("hours now: " + getHours)
         var getMinutes = timeNow.getMinutes();
+        console.log("minutes now: " + getMinutes)
         var timeNowinMinutes = (getHours * 60) + getMinutes
-        // Get next train time by verifying that next train is in the future nd not in the past
+        console.log("time now total in minutes is: " + timeNowinMinutes)
+        // Get next train time by verifying that next train is in the future and not in the past
         while (timeNowinMinutes > firstTrainMinutesTotal) {
             firstTrainMinutesTotal = parseInt(firstTrainMinutesTotal) + parseInt(frequency)
         }
         var nextTrainArrivalHour = parseInt(firstTrainMinutesTotal / 60)
+        console.log("next train hour arrive is: " + nextTrainArrivalHour)
         var minutesAway = firstTrainMinutesTotal - timeNowinMinutes
-        nextTrainArrivalMinutes = Math.abs(getMinutes + minutesAway - 60);
+        console.log("The next train is " + minutesAway + " minutes away")
+        nextTrainArrivalMinutes = (getMinutes + minutesAway);
+        if (nextTrainArrivalMinutes >= 60){
+            nextTrainArrivalMinutes = nextTrainArrivalMinutes - 60
+        }
+       
         if (nextTrainArrivalMinutes < 10) {
             nextTrainArrivalMinutes = "0" + nextTrainArrivalMinutes
         }
+        console.log("next train minutes arrive is " + nextTrainArrivalMinutes)
         // Add data to table inside html page
         var markup = "<tr><td>" + train + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrainArrivalHour + ":" + nextTrainArrivalMinutes + "</td><td>" + minutesAway + "</td></tr>";
 
